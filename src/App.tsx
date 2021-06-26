c6441b3aef3ef9b1e17a94d97f5747905be4f13a
Lauro Costa <joaolauro94@gmail.com>
2021-06-25 22:53:23 -0300

    created project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.scss";
+import { FiLink, FiUser, FiBook, FiSearch } from "react-icons/fi";
+import { api } from "./components/services/api";
+import { ChangeEvent, useState } from "react";
+
+type Hit = {
+  title: string;
+  url: string;
+  author: string;
+  objectID: string;
+  created_at: string;
+};
+
+type DataProps = {
+  hits: Hit[];
+};
 
 function App() {
+  const [hits, setHits] = useState<Hit[]>([]);
+  const [searchKey, setSearchKey] = useState<string>("")
+
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>){
+    const { value } = event.target; 
+    setSearchKey(value)
+  }
+  async function getBook(e: React.FormEvent) {
+    e.preventDefault();
+
+    const { data } = await api.get<DataProps>(`search?query=${searchKey}`);
+
+    const hits = data.hits.map((hit) => {
+      return {
+        title: hit.title,
+        url: hit.url,
+        author: hit.author,
+        objectID: hit.objectID,
+        created_at: hit.created_at
+      };
+    });
+    setHits(hits);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+    <header>
+      <div className="content">
+        <h1>Book Catalog</h1>
+
+        <form onSubmit={getBook} className="search">
+          <input 
+            type="text" 
+            id="searchKey" 
+            onChange={handleInputChange}
+          />
+          <button type="submit"><FiSearch size={22} color={"#FFF"}/></button>
+        </form>
+      </div>
+    </header>
+    
+    <main>
+
+      {hits.map((hit) => {
+        return (
+          <div key={hit.objectID} className="container">
+            <div>
+              <FiUser width={70} size={20} color={"#767676"}/>
+              <h1>{hit.author}</h1>
+            </div>
+
+            <div title={hit.title}>
+              <FiBook width={70} size={20} color={"#767676"}/>
+              <h1>{hit.title}</h1>
+            </div>
+            
+            <div title={hit.url}>
+              <FiLink width={80} size={20} color={"#767676"}/>
+              <a href={hit.url}>{hit.url}</a>
+            </div>
+          </div>
+        );
+      })}
+    </main>
+
+    </>
   );
 }
 
